6d24f874dc45aad9084b7fc24da3751ba802a538
Roland Prosek <Roland Prosek>
2017-03-13 23:04:02 +0100

    Refactored Router to bookRoutes.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://192.168.178.49/bookAPI'); // eslint-disable-line
 
-var Book = require('./models/bookModel');
+
 var app = express();
 var port = process.env.port || 3000;
 
@@ -12,56 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Routing
-var bookRouter = express.Router();
-
-bookRouter.route('/Books')
-    .post(function (req, res) {
-        var book = new Book(req.body);
-        book.save();
-        console.log("Sucessfully saved: _id" + book._id);
-        res.status(201).send(book);
-    })
-    .get(function (req, res) {
-
-        var query = [];
-        if (req.query.genre) {
-            query.genre = req.query.genre;
-        }
-
-
-        Book.find(query, function (err, books) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            } else {
-                if (query.genre) {
-                    console.log('Sucess: /api/Books with genre:' + query.genre);
-                }
-                else {
-                    console.log('Sucess: /api/Books');
-                }
-
-                res.json(books);
-            }
-        });
-        // var responseJson = {hello: "This is my Api"};
-        // res.json(responseJson);
-    });
-
-
-bookRouter.route('/Books/:bookId')
-    .get(function (req, res) {
-        console.log(req.params.bookId);
-        Book.findById(req.params.bookId, function (err, book) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(err);
-            } else {
-                console.log('Sucess: /api/Books/' + req.params.bookId);
-                res.json(book);
-            }
-        });
-    });
+var bookRouter = require('./routes/bookRoutes')();
 
 app.use('/api', bookRouter);
 
---
+++ routes/bookRoutes.js
@@ -0,0 +1,64 @@
+var express = require('express');
+
+
+
+var routes = function () {
+
+    var Book = require('../models/bookModel');
+    var bookRouter = express.Router();
+
+    bookRouter.route('/Books')
+        .post(function (req, res) {
+            var book = new Book(req.body);
+            book.save();
+            console.log("---\nSucess: saved _id : " + book._id);
+            res.status(201).send(book);
+        })
+        .get(function (req, res) {
+
+            //Allowed Filter Settings
+            var query = {};
+            if (req.query.genre) query.genre = req.query.genre;
+            if (req.query.title) query.title = req.query.title;
+            if (req.query.read) query.read = req.query.read;
+            if (req.query.author) query.author(req.query.author);
+
+            //Filtering by query
+            Book.find(query, function (err, books) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(err);
+                } else {
+                    if (Object.keys(query).length == 0) {
+                        console.log('---\nSucess: /api/Books was called without filters.');
+                    } else {
+                        console.log('---\nSucess: /api/Books was called with the following filters:');
+                        for (var key in query) {
+                            console.log('   Filter: ' + key + ' : ' + query[key]);
+                        }
+                    }
+                    res.json(books);
+                }
+            });
+        });
+
+
+    bookRouter.route('/Books/:bookId')
+        .get(function (req, res) {
+            console.log(req.params.bookId);
+            Book.findById(req.params.bookId, function (err, book) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(err);
+                } else {
+                    console.log('Sucess: /api/Books/' + req.params.bookId);
+                    res.json(book);
+                }
+            });
+        });
+
+    return bookRouter;
+};
+
+module.exports = routes;
+
